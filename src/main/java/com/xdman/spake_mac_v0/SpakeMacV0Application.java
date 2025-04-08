6477d765a7bb425e8cbe4a0574990883db757bd6
bao_ren.lim <merszerodev@gmail.com>
2025-04-08 09:57:55 +0800

    #NA REMOVE CACHING

--- src/main/java/com/xdman/spake_mac_v0/SpakeMacV0Application.java
+++ src/main/java/com/xdman/spake_mac_v0/SpakeMacV0Application.java
@@ -2,7 +2,6 @@ package com.xdman.spake_mac_v0;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 public class SpakeMacV0Application {
